27327dab605d33bb1cf9dced381148e7a40b1962
mendelabramzon <113777495+mendelabramzon@users.noreply.github.com>
2023-06-09 11:20:24 +0200

    Update mathjax.js

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -2,7 +2,7 @@ window.MathJax = {
     tex: {
       inlineMath: [["\\(", "\\)"]],
       displayMath: [["\\[", "\\]"]],
-      displayMath: [["$$\\", "\\$$"]], 
+      displayMath: [["$$\text{}", "\text{}$$"]], 
       processEscapes: true,
       processEnvironments: true
     },
